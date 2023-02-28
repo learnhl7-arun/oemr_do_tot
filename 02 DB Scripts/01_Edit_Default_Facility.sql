55df00a2fc92cb27b9f7f700155f430cde1d0c85
Arun Kumar P <learnhl7.arun@gmail.com>
2023-02-28 08:01:00 +0530

    Added Data Scripts

---
+++ 02 DB Scripts/01_Edit_Default_Facility.sql
@@ -0,0 +1,9 @@
+update facility f 
+set name = 'Dhanavanthari Clinic',
+    phone = '080-4401 1234',
+    street = 'BVK Iyengar Road',
+    city  = 'Bangalore',
+    state = 'Karnataka',
+    postal_code = '560004',
+    pos_code = '17'
+where id =3;
