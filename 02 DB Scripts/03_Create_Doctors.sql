9a51ef06e46906bc5621b86e63885f6e3cfd2b66
Arun Kumar P <learnhl7.arun@gmail.com>
2023-03-03 08:13:35 +0530

    Added Doctors. Edited Facility

---
+++ 02 DB Scripts/03_Create_Doctors.sql
@@ -0,0 +1,9 @@
+INSERT INTO users
+(username, authorized, fname, lname, mname, facility, facility_id, see_auth, active, taxonomy, calendar, physician_type, main_menu_role, patient_menu_role, billing_facility_id, billing_facility)
+VALUES('jatin', 1, 'Jatin', 'Malpani', '', 'Dhanavanthari Clinic', 3, 1, 1, '207Q00000X', 1, 'attending_physician', 'standard', 'standard', 3, 'Dhanavanthari Clinic');
+INSERT INTO users
+(username, authorized, fname, lname, mname, facility, facility_id, see_auth, active, taxonomy, calendar, physician_type, main_menu_role, patient_menu_role, billing_facility_id, billing_facility)
+VALUES('prashanth', 1, 'Prashanth', 'Issac', '', 'Dhanavanthari Clinic', 3, 1, 1, '207Q00000X', 1, 'general_physician', 'standard', 'standard', 3, 'Dhanavanthari Clinic');
+INSERT INTO users
+(username, authorized, fname, lname, mname, facility, facility_id, see_auth, active, taxonomy, calendar, physician_type, main_menu_role, patient_menu_role, billing_facility_id, billing_facility)
+VALUES('sharad', 1, 'Sharad', 'Gill', '', 'Dhanavanthari Clinic', 3, 1, 1, '207Q00000X', 1, '', 'standard', 'standard', 3, 'Dhanavanthari Clinic');
